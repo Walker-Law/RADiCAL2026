a8c1cfcc405e01b705756a6d36b8e0213239454b
Walker Law <170450824+Walker-Law@users.noreply.github.com>
2026-06-09 16:48:04 -0500

    Auto-update: 2026-06-09 16:48

--- RADiCALsim1/src/EventAction.cc
+++ RADiCALsim1/src/EventAction.cc
@@ -18,8 +18,8 @@ EventAction::EventAction() {
     fEdepMCP = 0.;
     fTimeMCP = kBigTime;
     fEdepPbGlass = 0.;
-    fTphFront.fill(kBigTime); fTphBack.fill(kBigTime);
-    fNphFront.fill(0);        fNphBack.fill(0);
+    fTphUp.fill(kBigTime); fTphDown.fill(kBigTime);
+    fNphUp.fill(0);        fNphDown.fill(0);
 }
 EventAction::~EventAction() {}
 
@@ -35,8 +35,8 @@ void EventAction::BeginOfEventAction(const G4Event*) {
     fEdepMCP = 0.;
     fTimeMCP = kBigTime;
     fEdepPbGlass = 0.;
-    fTphFront.fill(kBigTime); fTphBack.fill(kBigTime);
-    fNphFront.fill(0);        fNphBack.fill(0);
+    fTphUp.fill(kBigTime); fTphDown.fill(kBigTime);
+    fNphUp.fill(0);        fNphDown.fill(0);
 }
 
 void EventAction::EndOfEventAction(const G4Event*) {
