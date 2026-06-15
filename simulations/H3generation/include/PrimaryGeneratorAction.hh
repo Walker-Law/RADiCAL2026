57d312ef8b1a33d5fed2cd3bb6589ffcad8b46e9
Walker Law <170450824+Walker-Law@users.noreply.github.com>
2026-06-15 13:43:11 -0500

    Auto-update: 2026-06-15 13:43

---
+++ simulations/H3generation/include/PrimaryGeneratorAction.hh
@@ -0,0 +1,12 @@
+#pragma once
+#include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4ParticleGun.hh"
+
+class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction {
+public:
+    PrimaryGeneratorAction();
+    ~PrimaryGeneratorAction() override;
+    void GeneratePrimaries(G4Event*) override;
+private:
+    G4ParticleGun* fParticleGun;
+};
