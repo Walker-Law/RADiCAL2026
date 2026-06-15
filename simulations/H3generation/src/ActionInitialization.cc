dac3703adbbdb4c631166ba45f658fdad9acbe26
Walker Law <170450824+Walker-Law@users.noreply.github.com>
2026-06-15 14:01:12 -0500

    Auto-update: 2026-06-15 14:01

--- simulations/H3generation/src/ActionInitialization.cc
+++ simulations/H3generation/src/ActionInitialization.cc
@@ -1,6 +1,11 @@
 #include "ActionInitialization.hh"
 #include "PrimaryGeneratorAction.hh"
+#include "RunAction.hh"
+#include "SteppingAction.hh"
 
 void ActionInitialization::Build() const {
+    auto* runAction = new RunAction();
     SetUserAction(new PrimaryGeneratorAction());
+    SetUserAction(runAction);
+    SetUserAction(new SteppingAction(runAction));
 }
