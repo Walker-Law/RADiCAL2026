f5389e2526abdb32d490115d4facedfe8d12695d
Walker Law <170450824+Walker-Law@users.noreply.github.com>
2026-06-15 13:48:57 -0500

    Auto-update: 2026-06-15 13:48

--- simulations/H3generation/H3generation.cc
+++ simulations/H3generation/H3generation.cc
@@ -9,12 +9,13 @@
 #include "ActionInitialization.hh"
 
 int main(int argc, char** argv) {
-    auto runManager = G4RunManagerFactory::CreateRunManager();
+    // Must be created first on macOS so Qt owns the main thread
+    G4UIExecutive* uiExec = (argc == 1) ? new G4UIExecutive(argc, argv) : nullptr;
 
+    auto runManager = G4RunManagerFactory::CreateRunManager();
     runManager->SetUserInitialization(new DetectorConstruction());
     runManager->SetUserInitialization(new FTFP_BERT());
     runManager->SetUserInitialization(new ActionInitialization());
-
     runManager->Initialize();
 
     auto visManager = new G4VisExecutive();
@@ -25,7 +26,7 @@ int main(int argc, char** argv) {
     if (argc >= 2) {
         G4String macro = argv[1];
         if (macro.find("vis") != std::string::npos) {
-            auto uiExec = new G4UIExecutive(argc, argv);
+            uiExec = new G4UIExecutive(argc, argv);
             UI->ApplyCommand("/control/execute " + macro);
             uiExec->SessionStart();
             delete uiExec;
@@ -33,7 +34,6 @@ int main(int argc, char** argv) {
             UI->ApplyCommand("/control/execute " + macro);
         }
     } else {
-        auto uiExec = new G4UIExecutive(argc, argv);
         UI->ApplyCommand("/control/execute vis.mac");
         uiExec->SessionStart();
         delete uiExec;
