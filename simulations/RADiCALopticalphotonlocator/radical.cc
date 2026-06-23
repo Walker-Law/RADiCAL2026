08e1267bf31bcbe94e2f26c22d192449292f4c13
Walker Law <170450824+Walker-Law@users.noreply.github.com>
2026-06-23 13:50:41 -0500

    Auto-update: 2026-06-23 13:50

--- simulations/RADiCALopticalphotonlocator/radical.cc
+++ simulations/RADiCALopticalphotonlocator/radical.cc
@@ -69,7 +69,7 @@ int main(int argc, char** argv) {
         }
     } else {
         auto uiExec = new G4UIExecutive(argc, argv);
-        UI->ApplyCommand("/control/execute vis_purple.mac");
+        UI->ApplyCommand("/control/execute vis_corners.mac");
         uiExec->SessionStart();
         delete uiExec;
     }
