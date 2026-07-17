c4ee36a753516efd6fefbeae6e134657d8c0357b
Walker Law <170450824+Walker-Law@users.noreply.github.com>
2026-07-17 14:54:49 -0500

    Auto-update: 2026-07-17 14:54

--- simulations/RADiCALsimLuAG/src/DetectorConstruction.cc
+++ simulations/RADiCALsimLuAG/src/DetectorConstruction.cc
@@ -470,8 +470,6 @@ G4VPhysicalVolume* DetectorConstruction::Construct() {
 
         new G4PVPlacement(nullptr, xy + G4ThreeVector(0, 0, z_upstream),
                           logicTUpstream, "TCapUpstream_Phys", logicCalo, false, c-1);
-        new G4PVPlacement(nullptr, xy + G4ThreeVector(0, 0, z_upstream),
-                          logicBoreUpstream, "TCapBoreUp_Phys", logicCalo, false, c-1);
 
         new G4PVPlacement(nullptr, xy + G4ThreeVector(0, 0, z_wls),
                           logicTMidTube, "TCapMidTube_Phys", logicCalo, false, c-1);
@@ -481,8 +479,6 @@ G4VPhysicalVolume* DetectorConstruction::Construct() {
 
         new G4PVPlacement(nullptr, xy + G4ThreeVector(0, 0, z_downstream),
                           logicTDownstream, "TCapDownstream_Phys", logicCalo, false, c-1);
-        new G4PVPlacement(nullptr, xy + G4ThreeVector(0, 0, z_downstream),
-                          logicBoreDownstream, "TCapBoreDown_Phys", logicCalo, false, c-1);
 
         // photodetectors (copy number = corner index 0..3)
         new G4PVPlacement(nullptr, xy + G4ThreeVector(0, 0, zPDUpstream),
