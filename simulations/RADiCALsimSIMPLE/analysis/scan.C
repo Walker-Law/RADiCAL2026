46219b017667044fae8bbd1313d3a2708c8ba75f
Walker Law <170450824+Walker-Law@users.noreply.github.com>
2026-07-27 09:21:57 -0500

    Auto-update: 2026-07-27 09:21

--- simulations/RADiCALsimSIMPLE/analysis/scan.C
+++ simulations/RADiCALsimSIMPLE/analysis/scan.C
@@ -99,7 +99,7 @@ void scan(const char* dir = "build") {
         e->SetDirectory(nullptr);                            // detach from the file
         double muE, sgE, sgEErr;
         coreFitAndSave(e, Form("E_{LYSO} at %.0f GeV;E_{LYSO} (GeV);events", E[i]),
-                       Form("build/plots/fits/Elyso_E%.0fGeV.png", E[i]), muE, sgE, sgEErr);
+                       Form("%s/plots/fits/Elyso_E%.0fGeV.png", dir, E[i]), muE, sgE, sgEErr);
         delete e;
         resE[i]    = 100 * sgE / muE;
         resEerr[i] = resE[i] * sqrt(pow(sgEErr/sgE, 2));     // sigma error dominates
